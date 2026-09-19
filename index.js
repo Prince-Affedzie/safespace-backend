09b1ec2a4f18dfa1aa4400eee601f12592b5496c
Prince-Affeddzie <paffedzie001@st.ug.edu.gh>
2026-09-19 19:13:55 +0000

    error fix

--- index.js
+++ index.js
@@ -40,4 +40,7 @@ mongoose
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch((err) => console.log("MongoDB connection error:", err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
